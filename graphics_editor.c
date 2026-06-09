f3a5cf7e8323392746928185b4abca1831616c5c
jonithin236 <sinch73s@gmail.com>
2026-06-09 14:29:39 +0530

    Added graphics editor canvas

---
+++ graphics_editor.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define ROWS 20
+#define COLS 40
+
+char canvas[ROWS][COLS];
+
+void clearCanvas()
+{
+    int i, j;
+
+    for(i = 0; i < ROWS; i++)
+    {
+        for(j = 0; j < COLS; j++)
+        {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
+void displayCanvas()
+{
+    int i, j;
+
+    for(i = 0; i < ROWS; i++)
+    {
+        for(j = 0; j < COLS; j++)
+        {
+            printf("%c", canvas[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+void drawRectangle(int row, int col, int height, int width)
+{
+    int i, j;
+
+    for(i = row; i < row + height; i++)
+    {
+        for(j = col; j < col + width; j++)
+        {
+            canvas[i][j] = '*';
+        }
+    }
+}
+
+int main()
+{
+    clearCanvas();
+    displayCanvas();
+
+    return 0;
+}
